6fb96b0a93faa2fa70d367f0c7fe2248904ec7ff
Guilherme Rojas V. de Lima <guilhermehrojas@gmail.com>
2013-12-09 19:08:32 -0200

    Preparing the dojo to proposal: Coffee + Ajax + Graphic Signed-off By: Renan Fichberg <rfichberg@gmail.com>

--- app/assets/javascripts/module/graphic.js.coffee
+++ app/assets/javascripts/module/graphic.js.coffee
@@ -1,18 +1,5 @@
+# This class should generate the metric result graphic showing its container 
+# in metric result html and asking the metric history action of the modules 
+# controller to get its content. 
 class Module.Graphic
-  constructor: (@container, @metric_name, @module_id) ->
-    $('tr#'+@container).slideToggle('slow')
-    this.load()
 
-  load: ->
-    # Those two var are necessary so the jQuery callback can use them
-    # Otherwise the scope of the callback function is isolated
-    container = @container 
-    display = this.display
-
-    $.get '/modules/' + @module_id + '/metric_history',
-          metric_name: @metric_name
-          (data) ->
-            display(data,container)
-
-  display: (data, container) ->
-    $('div#'+container).html('<img id="' + container + '" src="data:image/png;base64,' + data + '" class="img-rounded"/>')
--- app/controllers/modules_controller.rb
+++ app/controllers/modules_controller.rb
@@ -1,17 +1,10 @@
 class ModulesController < ApplicationController
 
   # GET /modules/1/metric_history
+  # This should get the metric history of a given metric from the given module result 
+  # and return its graphic
   def metric_history
-    module_result = ModuleResult.new({ id: params[:id] })
-    metric_history = module_result.metric_history(params[:metric_name]) # pending: sort this hash.
-    dates = Array.new
-    values = Array.new
-    metric_history.keys.each do |date| 
-      dates.push date
-      values.push metric_history[date]
-    end
-    
-    send_data(Base64.encode64(graphic_for(values, dates)), type: 'image/png', filename: "#{params[:module_id]}-#{params[:metric_name]}.png")
+   
   end
 
   # POST /modules/1/tree
@@ -21,22 +14,10 @@ class ModulesController < ApplicationController
 
   private
 
+  # This should use Gruff gem to generate the graphic using an array with values of
+  # a given metric result and another array with its processed date.
   def graphic_for(values, dates)
-    graphic = Gruff::Line.new(400)
-    graphic.hide_title = true
-    graphic.hide_legend = true
-    graphic.theme = {
-      colors: ['grey'],
-      marker_color: 'black',
-      background_colors: '#fff',
-    }
-
-    graphic.labels = Hash[dates.each_with_index.map{ |date, index| [index, date.to_s]}]
-
-    graphic.data('Values', values)
-
-    graphic.to_blob
+    
   end
-  
 
 end
